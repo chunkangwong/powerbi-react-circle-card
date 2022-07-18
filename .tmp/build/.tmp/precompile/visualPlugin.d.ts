6ef283f3b3996825c06de75f5dc82d58063cee1a
ckwong <chunkang.wong94@gmail.com>
2022-07-19 01:15:50 +0800

    Initial commit

---
+++ .tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var reactCircleCard74EE668E374F4FD99ED6BAF16A673B9D_DEBUG: IVisualPlugin;
+export default reactCircleCard74EE668E374F4FD99ED6BAF16A673B9D_DEBUG;
